884969c0f0fd8f6f4359b55edb5d57f5023cf27d
mobius29 <sagjin0000@naver.com>
2024-11-23 01:20:27 +0900

    Updates

---
+++ assets/Input.stories-CKYo6b_3.js
@@ -0,0 +1,3 @@
+import{j as o}from"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";const a=()=>o.jsx("input",{}),p={title:"Input",component:a},r={args:{}};var t,s,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {}
+}`,...(e=(s=r.parameters)==null?void 0:s.docs)==null?void 0:e.source}}};const c=["Primary"];export{r as Primary,c as __namedExportsOrder,p as default};
