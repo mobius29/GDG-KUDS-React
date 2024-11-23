c268b382009c0c95f724225f328a25398647b534
mobius29 <sagjin0000@naver.com>
2024-11-23 16:38:15 +0900

    feat: initial Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,14 +4,32 @@ import { css } from '@emotion/react';
 import { classNames } from '../../utils/classNames';
 import { GLOBAL_PREFIX } from '../../constants/prefix';
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactNode;
+  colorType?: 'primary' | 'blue' | 'green' | 'yellow' | 'red';
+  size?: 'small' | 'medium' | 'large';
+}
 
-const Button = ({ className, ...rest }: ButtonProps) => {
-  return <button className={classNames([`${GLOBAL_PREFIX}-button`, className])} css={ButtonStyles} {...rest} />;
+// TODO: Button 구현. 아래 주석 지우고 시작할 것!
+// eslint-disable-next-line
+const Button = ({ className, colorType, icon, size, children, ...rest }: ButtonProps) => {
+  const BUTTON_PREFIX = `${GLOBAL_PREFIX}-button`;
+
+  return (
+    <button
+      className={classNames([BUTTON_PREFIX, `${BUTTON_PREFIX}-${colorType}`, className])}
+      css={ButtonStyles}
+      {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
 
 const ButtonStyles = css({
   backgroundColor: '#eeeeee',
+  '&:hover': {}, // hover style
+  '&:active': {}, // click style
+  '&:disabled': {}, // disabled style
 });
