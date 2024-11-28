ae2c1e12208ef58e7de2bf1d9bc30126e203e61b
rla-suedeng <sue.gus77@gmail.com>
2024-11-28 17:54:18 +0900

    mod: stories 수정

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,15 +1,23 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Input from './Input';
+import { fn } from '@storybook/test';
 
 const meta = {
   title: 'Input',
   component: Input,
+  tags: ['autodocs'],
+  args: { onChange: fn(), placeholder: 'Input' },
 } satisfies Meta<typeof Input>;
 
 export default meta;
-
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    inputType: 'primary',
+    customSize: 'small',
+    label: '',
+    helpText: '',
+    disabled: false,
+  },
 };
